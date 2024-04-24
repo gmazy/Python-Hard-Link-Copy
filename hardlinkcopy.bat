cf571fd559f1ce6c89451b79f89989983d58a88f
STRmods <73671814+STRmods@users.noreply.github.com>
2024-04-24 05:42:15 +0100

    -Multi Folder/File support
    -Fix for batch no input files message and consistency with python -Added support for selecting multiple folders and dropping onto script
    -Added support for one or more loose files being dropped onto the script with date inserted into file name before extension.

--- hardlinkcopy.bat
+++ hardlinkcopy.bat
@@ -1,15 +1,33 @@
 @echo off
+TITLE Python Hard Link Copier v 1.1
+setlocal enabledelayedexpansion
+
 set scriptdir=%~dp0
 set pythonpath="%scriptdir%\Python37\python.exe"
+set hardlink="%scriptdir%\program.py"
 
 if exist %pythonpath% (
-	rem Use embedded python
+    rem Use embedded python
 ) else (
-	rem Use Python install
-	set pythonpath="python"
+    rem Use Python install
+    set pythonpath="python"
 )
 
-:: Run script
-%pythonpath% "%scriptdir%\program.py" %1
+if "%~1" == "" (
+    color 04
+    echo Drag ^& Drop folders or loose files over this script to make copy using hardlinks
+    timeout 5
+    exit /b
+)
 
-timeout 3
+:loop
+if "%~1" neq "" (
+    if exist "%~f1\" (
+        %pythonpath% %hardlink% "%~f1"
+    ) else (
+        %pythonpath% %hardlink% "%~f1"
+    )
+    shift
+    goto loop
+)
+timeout 5
